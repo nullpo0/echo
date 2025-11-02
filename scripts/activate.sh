02387c4b6531ca94018590cbbdef730354d683bf
CHJPC\liker <likercho63@gmail.com>
2025-11-02 16:30:03 +0900

    .

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -7,9 +7,14 @@ CONTAINER_NAME="pgDB"
 STATUS=$(docker ps -a --filter "name=^${CONTAINER_NAME}$" --format "{{.Status}}")
 
 if [[ $STATUS == *"Up"* ]]; then
+    :
 else
     if [[ -n $STATUS ]]; then
         docker start "$CONTAINER_NAME"
+    else
+        echo not exist container. please download postgreSQL image.
+    fi
+fi
 
 cd server
 
